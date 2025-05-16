8f910725aa5874d0d65997958b6c1a59ddf6facb
WillyAP <your_email@example.com>
2025-05-16 21:12:21 +0700

    update konten

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/home.jsx'
 import About from './pages/about.jsx'
 import Product from './pages/product.jsx'
 import Product1 from './pages/product1.jsx'
+import Product2 from './pages/product2.jsx'
 import Why from './pages/why.jsx'
 import Contact from './pages/contact.jsx'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<About/>} />
           <Route path="/product" element={<Product/>} />
           <Route path="/product1" element={<Product1/>} />
+          <Route path="/product2" element={<Product2/>} />
           <Route path="/why" element={<Why/>} />
           <Route path="/contact" element={<Contact/>}/>
         </Routes>
